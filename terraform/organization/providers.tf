ad384c176277c1a792955e57ca7f9358775a667c
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-18 14:49:27 -0300

    Initial commit

---
+++ terraform/organization/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+
+  cloud {
+    organization = "letsrockthefuture"
+
+    workspaces {
+      name = "api-development-us-east-1"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
---
+++ terraform/organization/variables.tf
@@ -0,0 +1,4 @@
+variable "github_token" {
+  description = "The GitHub token of the user that has access to the organization."
+  type        = "string"
+}
