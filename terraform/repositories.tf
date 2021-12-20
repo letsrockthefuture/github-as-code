6e5ee5fcd61777c013cefd569b6d47528490e1d9
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-20 10:17:37 -0300

    Initial commit

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -5,185 +5,276 @@
 #   - add the team to the repository and grant admin permissions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# module "golang_worker" {
-#   source  = "mineiros-io/repository/github"
-#   version = "~> 0.11.0"
+module "github" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
 
-#   module_depends_on = [
-#     module.reviewers,
-#   ]
+  module_depends_on = [
+    module.reviewers,
+  ]
 
-#   name        = "golang-worker"
-#   description = "A description of the repository."
-#   # homepage_url       = "https://github.com/mineiros-io"
-#   visibility         = "public"
-#   has_issues         = true
-#   has_projects       = false
-#   has_wiki           = false
-#   allow_merge_commit = true
-#   allow_rebase_merge = false
-#   allow_squash_merge = false
-#   has_downloads      = false
-#   auto_init          = true
-#   gitignore_template = "Terraform"
-#   license_template   = "apache-2.0"
+  name        = ".github"
+  description = "A description of the repository."
+  # homepage_url       = "https://github.com/mineiros-io"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
 
-#   topics = [
-#     "golang-worker",
-#     "terraform",
-#   ]
+  topics = [
+    "github",
+    "github-actions",
+    "terraform",
+  ]
 
-#   maintain_team_ids = [
-#     module.reviewers.id,
-#   ]
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
 
-#   push_team_ids = [
-#     module.developers.id,
-#   ]
+  push_team_ids = [
+    module.developers.id,
+  ]
 
-#   webhooks = [
-#     {
-#       active = true
+  webhooks = [
+    {
+      active = true
 
-#       events = [
-#         "issues",
-#       ]
+      events = [
+        "issues",
+      ]
 
-#       url          = "https://example.com/events"
-#       content_type = "application/json"
-#       insecure_ssl = false
-#       secret       = "sososecret"
-#     },
-#   ]
+      url          = "https://example.com/events"
+      content_type = "application/json"
+      insecure_ssl = false
+      secret       = "sososecret"
+    },
+  ]
 
-#   # admin_collaborators = [
-#   #   "danielvincenzi",
-#   # ]
+  # admin_collaborators = [
+  #   "danielvincenzi",
+  # ]
 
-#   branch_protections_v3 = [
-#     {
-#       branch                 = "main"
-#       enforce_admins         = true
-#       require_signed_commits = true
+  branch_protections_v3 = [
+    {
+      branch                 = "main"
+      enforce_admins         = true
+      require_signed_commits = true
 
-#       required_status_checks = {
-#         strict = true
+      required_status_checks = {
+        strict = true
 
-#         # contexts = [
-#         #   "ci/travis"
-#         # ]
-#       }
+        # contexts = [
+        #   "ci/travis"
+        # ]
+      }
 
-#       required_pull_request_reviews = {
-#         dismiss_stale_reviews = true
+      required_pull_request_reviews = {
+        dismiss_stale_reviews = true
 
-#         # dismissal_teams = [
-#         #   github_team.team.slug,
-#         # ]
+        # dismissal_teams = [
+        #   github_team.team.slug,
+        # ]
 
-#         require_code_owner_reviews      = true
-#         required_approving_review_count = 1
-#       }
+        require_code_owner_reviews      = true
+        required_approving_review_count = 1
+      }
 
-#       # restrictions = {
+      # restrictions = {
 
-#       #   teams = [
-#       #     github_team.team.slug,
-#       #   ]
-#       # }
-#     },
-#   ]
-# }
+      #   teams = [
+      #     github_team.team.slug,
+      #   ]
+      # }
+    },
+  ]
+}
 
-# module "terraform_cloud" {
-#   source  = "mineiros-io/repository/github"
-#   version = "~> 0.11.0"
+module "golang_worker" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
 
-#   module_depends_on = [
-#     module.reviewers,
-#   ]
+  module_depends_on = [
+    module.reviewers,
+  ]
 
-#   name        = "terraform-cloud"
-#   description = "A description of the repository."
-#   # homepage_url       = "https://github.com/mineiros-io"
-#   visibility         = "public"
-#   has_issues         = true
-#   has_projects       = false
-#   has_wiki           = false
-#   allow_merge_commit = true
-#   allow_rebase_merge = false
-#   allow_squash_merge = false
-#   has_downloads      = false
-#   auto_init          = true
-#   gitignore_template = "Terraform"
-#   license_template   = "apache-2.0"
+  name        = "golang-worker"
+  description = "A description of the repository."
+  # homepage_url       = "https://github.com/mineiros-io"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
 
-#   topics = [
-#     "terraform-cloud",
-#     "terraform",
-#   ]
+  topics = [
+    "golang-worker",
+    "terraform",
+  ]
 
-#   maintain_team_ids = [
-#     module.reviewers.id,
-#   ]
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
 
-#   push_team_ids = [
-#     module.developers.id,
-#   ]
+  push_team_ids = [
+    module.developers.id,
+  ]
 
-#   webhooks = [
-#     {
-#       active = true
+  webhooks = [
+    {
+      active = true
 
-#       events = [
-#         "issues",
-#       ]
+      events = [
+        "issues",
+      ]
 
-#       url          = "https://example.com/events"
-#       content_type = "application/json"
-#       insecure_ssl = false
-#       secret       = "sososecret"
-#     },
-#   ]
+      url          = "https://example.com/events"
+      content_type = "application/json"
+      insecure_ssl = false
+      secret       = "sososecret"
+    },
+  ]
 
-#   # admin_collaborators = [
-#   #   "danielvincenzi",
-#   # ]
+  # admin_collaborators = [
+  #   "danielvincenzi",
+  # ]
 
-#   branch_protections_v3 = [
-#     {
-#       branch                 = "main"
-#       enforce_admins         = true
-#       require_signed_commits = true
+  branch_protections_v3 = [
+    {
+      branch                 = "main"
+      enforce_admins         = true
+      require_signed_commits = true
 
-#       required_status_checks = {
-#         strict = true
+      required_status_checks = {
+        strict = true
 
-#         # contexts = [
-#         #   "ci/travis"
-#         # ]
-#       }
+        # contexts = [
+        #   "ci/travis"
+        # ]
+      }
 
-#       required_pull_request_reviews = {
-#         dismiss_stale_reviews = true
+      required_pull_request_reviews = {
+        dismiss_stale_reviews = true
 
-#         # dismissal_teams = [
-#         #   github_team.team.slug,
-#         # ]
+        # dismissal_teams = [
+        #   github_team.team.slug,
+        # ]
 
-#         require_code_owner_reviews      = true
-#         required_approving_review_count = 1
-#       }
+        require_code_owner_reviews      = true
+        required_approving_review_count = 1
+      }
 
-#       # restrictions = {
+      # restrictions = {
 
-#       #   teams = [
-#       #     github_team.team.slug,
-#       #   ]
-#       # }
-#     },
-#   ]
-# }
+      #   teams = [
+      #     github_team.team.slug,
+      #   ]
+      # }
+    },
+  ]
+}
+
+module "terraform_cloud" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
+
+  module_depends_on = [
+    module.reviewers,
+  ]
+
+  name        = "terraform-cloud"
+  description = "A description of the repository."
+  # homepage_url       = "https://github.com/mineiros-io"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
+
+  topics = [
+    "terraform-cloud",
+    "terraform",
+  ]
+
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
+
+  push_team_ids = [
+    module.developers.id,
+  ]
+
+  webhooks = [
+    {
+      active = true
+
+      events = [
+        "issues",
+      ]
+
+      url          = "https://example.com/events"
+      content_type = "application/json"
+      insecure_ssl = false
+      secret       = "sososecret"
+    },
+  ]
+
+  # admin_collaborators = [
+  #   "danielvincenzi",
+  # ]
+
+  branch_protections_v3 = [
+    {
+      branch                 = "main"
+      enforce_admins         = true
+      require_signed_commits = true
+
+      required_status_checks = {
+        strict = true
+
+        # contexts = [
+        #   "ci/travis"
+        # ]
+      }
+
+      required_pull_request_reviews = {
+        dismiss_stale_reviews = true
+
+        # dismissal_teams = [
+        #   github_team.team.slug,
+        # ]
+
+        require_code_owner_reviews      = true
+        required_approving_review_count = 1
+      }
+
+      # restrictions = {
+
+      #   teams = [
+      #     github_team.team.slug,
+      #   ]
+      # }
+    },
+  ]
+}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # TEAM
