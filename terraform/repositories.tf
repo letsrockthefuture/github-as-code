808a4687edaff4b5af7f0f6a8c2cbf6b4b3607e7
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-23 11:34:26 -0300

    Initial commit

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -332,6 +332,113 @@ resource "github_repository_environment" "terraform_workspaces_production" {
   ]
 }
 
+module "makefiles" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
+
+  module_depends_on = [
+    module.reviewers,
+  ]
+
+  name        = "makefiles"
+  description = "A collection of Makefiles to facilitate building Terraform, Golang projects, Dockerfiles, and more."
+  # homepage_url       = "https://github.com/mineiros-io"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  # gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
+
+  topics = [
+    "makefiles",
+  ]
+
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
+
+  push_team_ids = [
+    module.developers.id,
+  ]
+
+  # webhooks = [
+  #   {
+  #     active = true
+
+  #     events = [
+  #       "issues",
+  #     ]
+
+  #     url          = "https://example.com/events"
+  #     content_type = "application/json"
+  #     insecure_ssl = false
+  #     secret       = "sososecret"
+  #   },
+  # ]
+
+  # admin_collaborators = [
+  #   "danielvincenzi",
+  # ]
+
+  # branch_protections_v3 = [
+  #   {
+  #     branch                 = "main"
+  #     enforce_admins         = true
+  #     require_signed_commits = true
+
+  #     required_status_checks = {
+  #       strict = true
+
+  #       # contexts = [
+  #       #   "ci/travis"
+  #       # ]
+  #     }
+
+  #     required_pull_request_reviews = {
+  #       dismiss_stale_reviews = true
+
+  #       # dismissal_teams = [
+  #       #   github_team.team.slug,
+  #       # ]
+
+  #       require_code_owner_reviews      = true
+  #       required_approving_review_count = 1
+  #     }
+
+  #     # restrictions = {
+
+  #     #   teams = [
+  #     #     github_team.team.slug,
+  #     #   ]
+  #     # }
+  #   },
+  # ]
+}
+
+resource "github_repository_environment" "makefiles_production" {
+  environment = "production"
+  repository  = "makefiles"
+  reviewers {
+    teams = [
+      module.reviewers.id,
+    ]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+
+  module_depends_on = [
+    module.makefiles.id,
+  ]
+}
+
 module "golang_worker" {
   source  = "mineiros-io/repository/github"
   version = "~> 0.11.0"
