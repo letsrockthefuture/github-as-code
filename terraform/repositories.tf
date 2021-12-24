785b5e49d3ee7b8f749aba38d4e950e57c8e5483
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-24 07:02:39 -0300

    Initial commit

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -31,7 +31,6 @@ module "github" {
   topics = [
     "github",
     "github-actions",
-    "terraform",
   ]
 
   maintain_team_ids = [
@@ -110,7 +109,116 @@ resource "github_repository_environment" "github_production" {
   }
 
   depends_on = [
-    module.github.id,
+    module.github,
+  ]
+}
+
+module "reusable_workflows" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
+
+  module_depends_on = [
+    module.reviewers,
+  ]
+
+  name        = "reusable-workflows"
+  description = "A description of the repository."
+  # homepage_url       = "https://github.com/mineiros-io"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
+
+  topics = [
+    "github-actions",
+    "github-workflows",
+    "reusable-workflows",
+  ]
+
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
+
+  push_team_ids = [
+    module.developers.id,
+  ]
+
+  # webhooks = [
+  #   {
+  #     active = true
+
+  #     events = [
+  #       "issues",
+  #     ]
+
+  #     url          = "https://example.com/events"
+  #     content_type = "application/json"
+  #     insecure_ssl = false
+  #     secret       = "sososecret"
+  #   },
+  # ]
+
+  # admin_collaborators = [
+  #   "danielvincenzi",
+  # ]
+
+  # branch_protections_v3 = [
+  #   {
+  #     branch                 = "main"
+  #     enforce_admins         = true
+  #     require_signed_commits = true
+
+  #     required_status_checks = {
+  #       strict = true
+
+  #       # contexts = [
+  #       #   "ci/travis"
+  #       # ]
+  #     }
+
+  #     required_pull_request_reviews = {
+  #       dismiss_stale_reviews = true
+
+  #       # dismissal_teams = [
+  #       #   github_team.team.slug,
+  #       # ]
+
+  #       require_code_owner_reviews      = true
+  #       required_approving_review_count = 1
+  #     }
+
+  #     # restrictions = {
+
+  #     #   teams = [
+  #     #     github_team.team.slug,
+  #     #   ]
+  #     # }
+  #   },
+  # ]
+}
+
+resource "github_repository_environment" "reusable_workflows_production" {
+  environment = "production"
+  repository  = "reusable-workflows"
+  reviewers {
+    teams = [
+      module.reviewers.id,
+    ]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+
+  depends_on = [
+    module.reusable_workflows,
   ]
 }
 
@@ -220,7 +328,7 @@ resource "github_repository_environment" "terraform_repository_template_producti
   }
 
   depends_on = [
-    module.terraform_repository_template.id,
+    module.terraform_repository_template,
   ]
 }
 
@@ -328,7 +436,7 @@ resource "github_repository_environment" "terraform_workspaces_production" {
   }
 
   depends_on = [
-    module.terraform_workspaces.id,
+    module.terraform_workspaces,
   ]
 }
 
@@ -435,7 +543,7 @@ resource "github_repository_environment" "makefiles_production" {
   }
 
   depends_on = [
-    module.makefiles.id,
+    module.makefiles,
   ]
 }
 
@@ -543,7 +651,7 @@ module "golang_worker" {
 #   }
 
 #   depends_on = [
-#     module.golang_worker.id,
+#     module.golang_worker,
 #   ]
 # }
 
@@ -669,7 +777,7 @@ resource "github_repository_environment" "terraform_tfe_workspaces_production" {
   }
 
   depends_on = [
-    module.terraform_tfe_workspaces.id,
+    module.terraform_tfe_workspaces,
   ]
 }
 
