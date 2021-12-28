1c7037aa0e6c21a69b80fb4640df35b86cf523a1
Daniel Vincenzi <danielvincenzi@icloud.com>
2021-12-28 07:30:57 -0300

    Initial commit

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -682,3 +682,98 @@ resource "github_repository_environment" "terraform_tfe_workspaces_production" {
     module.terraform_tfe_workspaces,
   ]
 }
+
+#############################################################
+### https://github.com/letsrockthefuture/aws-environments ###
+#############################################################
+
+module "aws_envirnoments" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.11.0"
+
+  module_depends_on = [
+    module.reviewers,
+    module.dependabot_reviewers,
+  ]
+
+  name               = "aws-environments"
+  description        = "A project that brings together the deployment of necessary AWS resources for our development & production environments."
+  homepage_url       = "https://github.com/letsrockthefuture/aws-environments"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
+
+  topics = [
+    "aws-environments",
+    "terraform",
+  ]
+
+  maintain_team_ids = [
+    module.reviewers.id,
+    module.dependabot_reviewers.id,
+  ]
+
+  push_team_ids = [
+    module.developers.id,
+  ]
+
+  # branch_protections_v3 = [
+  #   {
+  #     branch                 = "main"
+  #     enforce_admins         = true
+  #     require_signed_commits = true
+
+  #     required_status_checks = {
+  #       strict = true
+
+  #       # contexts = [
+  #       #   "ci/travis"
+  #       # ]
+  #     }
+
+  #     required_pull_request_reviews = {
+  #       dismiss_stale_reviews = true
+
+  #       # dismissal_teams = [
+  #       #   github_team.team.slug,
+  #       # ]
+
+  #       require_code_owner_reviews      = true
+  #       required_approving_review_count = 1
+  #     }
+
+  #     # restrictions = {
+
+  #     #   teams = [
+  #     #     github_team.team.slug,
+  #     #   ]
+  #     # }
+  #   },
+  # ]
+}
+
+resource "github_repository_environment" "aws_environments_production" {
+  environment = "production"
+  repository  = "aws-environments"
+  reviewers {
+    teams = [
+      module.reviewers.id,
+    ]
+  }
+  deployment_branch_policy {
+    protected_branches     = true
+    custom_branch_policies = false
+  }
+
+  depends_on = [
+    module.aws_environments,
+  ]
+}
