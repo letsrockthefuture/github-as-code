abb65cc75e96fd20f2fc1dab646b38babc880e90
Daniel Vincenzi <danielvincenzi@icloud.com>
2022-02-27 17:22:46 -0300

    Initial commit

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -743,3 +743,99 @@ module "aws_stacks" {
 #     module.aws_environments,
 #   ]
 # }
+
+module "terraform_aws_eks" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.14.0"
+
+  module_depends_on = [
+    module.reviewers,
+  ]
+
+  name               = "terraform-aws-eks"
+  description        = "Terraform module to create an Elastic Kubernetes (EKS) cluster and associated worker instances on AWS."
+  homepage_url       = "https://github.com/letsrockthefuture/terraform-aws-eks"
+  visibility         = "public"
+  has_issues         = true
+  has_projects       = false
+  has_wiki           = false
+  allow_merge_commit = true
+  allow_rebase_merge = false
+  allow_squash_merge = false
+  has_downloads      = false
+  auto_init          = true
+  gitignore_template = "Terraform"
+  license_template   = "apache-2.0"
+
+  template = {
+    owner      = "letsrockthefuture"
+    repository = "terraform-module-template"
+  }
+
+  topics = [
+    "terraform-aws-eks",
+    "terraform",
+  ]
+
+  maintain_team_ids = [
+    module.reviewers.id,
+  ]
+
+  push_team_ids = [
+    module.developers.id,
+  ]
+
+  vulnerability_alerts = true
+
+  # branch_protections_v3 = [
+  #   {
+  #     branch                 = "main"
+  #     enforce_admins         = true
+  #     require_signed_commits = true
+
+  #     required_status_checks = {
+  #       strict = true
+
+  #       # contexts = [
+  #       #   "ci/travis"
+  #       # ]
+  #     }
+
+  #     required_pull_request_reviews = {
+  #       dismiss_stale_reviews = true
+
+  #       # dismissal_teams = [
+  #       #   github_team.team.slug,
+  #       # ]
+
+  #       require_code_owner_reviews      = true
+  #       required_approving_review_count = 1
+  #     }
+
+  #     # restrictions = {
+
+  #     #   teams = [
+  #     #     github_team.team.slug,
+  #     #   ]
+  #     # }
+  #   },
+  # ]
+}
+
+# resource "github_repository_environment" "terraform_repository_template_production" {
+#   environment = "production"
+#   repository  = "terraform-repository-template"
+#   reviewers {
+#     teams = [
+#       module.reviewers.id,
+#     ]
+#   }
+#   deployment_branch_policy {
+#     protected_branches     = true
+#     custom_branch_policies = false
+#   }
+
+#   depends_on = [
+#     module.terraform_repository_template,
+#   ]
+# }
